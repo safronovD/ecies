3f1157c63a9e5bca04e5b929068d60d21def74b8
Danil Safronov <danil_safronov@dell.com>
2023-07-20 12:53:31 +0200

    try prime256v1

--- secp256k1_cgo.go
+++ secp256k1_cgo.go
@@ -5,10 +5,9 @@ package eciesgo
 
 import (
 	"crypto/elliptic"
-
-	"github.com/ethereum/go-ethereum/crypto/secp256k1"
+	//"github.com/ethereum/go-ethereum/crypto/secp256k1"
 )
 
 func getCurve() elliptic.Curve {
-	return secp256k1.S256()
+	return elliptic.P256()
 }
--- secp256k1_nocgo.go
+++ secp256k1_nocgo.go
@@ -5,10 +5,8 @@ package eciesgo
 
 import (
 	"crypto/elliptic"
-
-	"github.com/decred/dcrd/dcrec/secp256k1/v4"
 )
 
 func getCurve() elliptic.Curve {
-	return secp256k1.S256()
+	return elliptic.P256()
 }
